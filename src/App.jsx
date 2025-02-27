31bee495618661ed23503db97a26ed5d3fa7b749
JDY <wjsehdud@nate.com>
2025-02-27 19:44:32 +0900

    Project: 프로필 페이지 컴포넌트 추가 및 라우팅 수정: 사용자 상태 관리 추가

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,19 @@
 import { Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
-import Signup from "./pages/Signup";
 import Login from "./pages/Login";
+import SignUp from "./pages/Signup";
+import Profile from "./pages/Profile";
+import { useState } from "react";
 
 const App = () => {
+  const [user, setUser] = useState(null);
+
   return (
     <Routes>
       <Route path="/" element={<Home />} />
-      <Route path="/signup" element={<Signup />} />
-      <Route path="/login" element={<Login />} />
-      <Route path="/" element={<Home />} />
+      <Route path="/signup" element={<SignUp />} />
+      <Route path="/login" element={<Login setUser={setUser} />} />
+      <Route path="/profile" element={<Profile user={user} />} />
       <Route path="/" element={<Home />} />
       <Route path="/" element={<Home />} />
       <Route path="/" element={<Home />} />
---
+++ src/pages/Profile.jsx
@@ -0,0 +1,10 @@
+const Profile = ({user}) => {
+  console.log(user);
+  return (
+    <div>
+      <h1>프로필 페이지 입니다.</h1>
+    </div>
+  );
+};
+
+export default Profile;
